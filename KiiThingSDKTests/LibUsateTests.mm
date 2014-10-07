51040efc4d314754cfb27f93a07adff78df60dcd
satoshi <satoshi.kumano@kii.com>
2014-10-07 15:32:04 +0900

    Add skeleton of Object CRUD.

--- KiiThingSDKTests/LibUsateTests.mm
+++ KiiThingSDKTests/LibUsateTests.mm
@@ -9,6 +9,7 @@
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 #import "jansson.h"
+#import "curl.h"
 
 @interface KiiThingSDKTests : XCTestCase
 
@@ -24,7 +25,7 @@
     [super tearDown];
 }
 
-- (void)testExample {
+- (void)testExampleJannson {
     json_t* obj = json_object();
     json_t* str = json_string("huga");
     json_object_set(obj, "hoge", str);
@@ -34,5 +35,21 @@
     XCTAssert(YES, @"Pass");
 }
 
+size_t callbackWrite(char *ptr, size_t size, size_t nmemb)
+{
+    int dataLen = size * nmemb;
+    NSLog(@"received: %s", ptr);
+    return dataLen;
+}
+
+- (void)testExampleCurl {
+    CURL* curl = curl_easy_init();
+    curl_easy_setopt(curl, CURLOPT_URL, "http://www.google.com");
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callbackWrite);
+    CURLcode res = curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+    curl = NULL;
+    NSLog(@"curl res %d", res);
+}
 
 @end
