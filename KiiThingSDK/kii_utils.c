9fb62857f80372780f41d1087db6224a009efa96
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2014-10-20 18:31:07 +0900

    kii_utils files were added.

---
+++ KiiThingSDK/kii_utils.c
@@ -0,0 +1,8 @@
+//
+//  kii_utils.c
+//  KiiThingSDK
+//
+//  Copyright (c) 2014年 Kii. All rights reserved.
+//
+
+#include "kii_utils.h"
---
+++ KiiThingSDK/kii_utils.h
@@ -0,0 +1,13 @@
+//
+//  kii_utils.h
+//  KiiThingSDK
+//
+//  Copyright (c) 2014年 Kii. All rights reserved.
+//
+
+#ifndef __KiiThingSDK__kii_utils__
+#define __KiiThingSDK__kii_utils__
+
+#include <stdio.h>
+
+#endif /* defined(__KiiThingSDK__kii_utils__) */
