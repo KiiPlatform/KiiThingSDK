bcba9e4150a6050c0c95f2dd6f7f0dbc719f6051
naoki.akimoto <naoki.akimoto@muraoka-design.com>
2014-11-11 14:04:30 +0900

    parse errorCode in response.

--- KiiThingSDK/kii_cloud.c
+++ KiiThingSDK/kii_cloud.c
@@ -367,6 +367,7 @@ kii_error_code_t prv_execute_curl(CURL* curl,
             } else {
                 const kii_char_t* error_code = NULL;
                 json_t* errJson = NULL;
+                json_t* errorCodeJson = NULL;
                 if (*response_body != NULL) {
                     json_error_t jErr;
                     errJson = json_loads(*response_body, 0, &jErr);
@@ -374,7 +375,12 @@ kii_error_code_t prv_execute_curl(CURL* curl,
                         return KIIE_LOWMEMORY;
                     }
                 }
-                error_code = json_string_value(errJson);
+                errorCodeJson = json_object_get(errJson, "errorCode");
+                if (errorCodeJson != NULL) {
+                    error_code = json_string_value(errorCodeJson);
+                } else {
+                    error_code = *response_body;
+                }
                 prv_kii_set_info_in_error(error, (int)(*response_status_code),
                         error_code);
                 json_decref(errJson);
