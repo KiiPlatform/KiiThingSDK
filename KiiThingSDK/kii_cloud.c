130dcbc2d114dd2a2d3dee294f31ad56941464b4
naoki.akimoto <naoki.akimoto@muraoka-design.com>
2014-11-14 15:09:00 +0900

    divide kii_patch_object.

--- KiiThingSDK/kii_cloud.c
+++ KiiThingSDK/kii_cloud.c
@@ -853,6 +853,36 @@ ON_EXIT:
     return ret;
 }
 
+kii_error_code_t parse_patch_object_response(
+	long respCode,
+	const json_t* respHdr,
+	kii_char_t** out_etag,
+	kii_error_t* err)
+{
+    kii_error_code_t ret = KIIE_FAIL;
+
+    /* Check response header */
+    if (out_etag != NULL && respHdr != NULL) {
+        const kii_char_t* etag = json_string_value(json_object_get(respHdr,
+		    "etag"));
+        if (etag != NULL) {
+            *out_etag = kii_strdup(etag);
+            if (*out_etag == NULL) {
+                ret = KIIE_LOWMEMORY;
+            } else {
+		ret = KIIE_OK;
+	    }
+        } else {
+            prv_kii_set_info_in_error(err, (int)respCode, KII_ECODE_PARSE);
+            ret = KIIE_FAIL;
+        }
+    } else {
+	ret = KIIE_OK;
+    }
+
+    return ret;
+}
+
 kii_error_code_t kii_patch_object(kii_app_t app,
                                   const kii_char_t* access_token,
                                   const kii_bucket_t bucket,
@@ -924,21 +954,7 @@ kii_error_code_t kii_patch_object(kii_app_t app,
         goto ON_EXIT;
     }
 
-    /* Check response header */
-    if (out_etag != NULL && respHdr != NULL) {
-        const kii_char_t* etag = json_string_value(json_object_get(respHdr, "etag"));
-        if (etag != NULL) {
-            *out_etag = kii_strdup(etag);
-            if (*out_etag == NULL) {
-                ret = KIIE_LOWMEMORY;
-                goto ON_EXIT;
-            }
-        } else {
-            prv_kii_set_info_in_error(&err, (int)respCode, KII_ECODE_PARSE);
-            ret = KIIE_FAIL;
-            goto ON_EXIT;
-        }
-    }
+    ret = parse_patch_object_response(respCode, respHdr, out_etag, &err);
 
 ON_EXIT:
     M_KII_FREE_NULLIFY(reqUrl);
