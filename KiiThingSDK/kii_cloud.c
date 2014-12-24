710a8c80817825c4efb14cfd02b1261b98d254db
naoki.akimoto <naoki.akimoto@muraoka-design.com>
2014-12-24 19:41:05 +0900

    add http interface.

--- KiiThingSDK/kii_cloud.c
+++ KiiThingSDK/kii_cloud.c
@@ -15,6 +15,91 @@
 #include "kii_prv_utils.h"
 #include "kii_prv_types.h"
 
+static kii_error_code_t prv_http_delete(
+        const kii_char_t* url,
+        const kii_app_t* app,
+        const kii_char_t* access_token,
+        long* response_status_code,
+        kii_char_t** response_body,
+        kii_error_t* error)
+{
+    // TODO: implement me.
+    return KIIE_FAIL;
+}
+
+static kii_error_code_t prv_http_get(
+        const kii_char_t* url,
+        const kii_app_t* app,
+        const kii_char_t* access_token,
+        long* response_status_code,
+        json_t** response_headers,
+        kii_char_t** response_body,
+        kii_error_t* error)
+{
+    // TODO: implement me.
+    return KIIE_FAIL;
+}
+
+static kii_error_code_t prv_http_head(
+        const kii_char_t* url,
+        const kii_app_t* app,
+        const kii_char_t* access_token,
+        long* response_status_code,
+        kii_char_t** response_body,
+        kii_error_t* error)
+{
+    // TODO: implement me.
+    return KIIE_FAIL;
+}
+
+static kii_error_code_t prv_http_patch(
+        const kii_char_t* url,
+        const kii_app_t* app,
+        const kii_char_t* access_token,
+        const kii_char_t* content_type,
+        const kii_char_t* opt_etag,
+        const kii_char_t* request_body,
+        long* response_status_code,
+        json_t** response_headers,
+        kii_char_t** response_body,
+        kii_error_t* error)
+{
+    // TODO: implement me.
+    return KIIE_FAIL;
+}
+
+static kii_error_code_t prv_http_post(
+        const kii_char_t* url,
+        const kii_app_t* app,
+        const kii_char_t* access_token,
+        const kii_char_t* content_type,
+        const kii_char_t* request_body,
+        long* response_status_code,
+        json_t** response_headers,
+        kii_char_t** response_body,
+        kii_error_t* error)
+{
+    // TODO: implement me.
+    return KIIE_FAIL;
+}
+
+static kii_error_code_t prv_http_put(
+        const kii_char_t* url,
+        const kii_app_t* app,
+        const kii_char_t* access_token,
+        const kii_char_t* content_type,
+        const kii_char_t* opt_etag,
+        kii_bool_t if_none_match,
+        const kii_char_t* request_body,
+        long* response_status_code,
+        json_t** response_headers,
+        kii_char_t** response_body,
+        kii_error_t* error)
+{
+    // TODO: implement me.
+    return KIIE_FAIL;
+}
+
 kii_error_code_t kii_global_init(void)
 {
     CURLcode r = curl_global_init(CURL_GLOBAL_ALL);
